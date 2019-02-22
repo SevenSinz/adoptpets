51863b9c2bf5ac543e17dedf90f52c14d6ced89d
Ethan Shen <ethanshen.wu@gmail.com>
2019-02-22 14:55:52 -0800

    validation of inputs

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, redirect
 # , flash
 from flask_wtf import FlaskForm
+from flask_wtf.html5 import URLField
 from flask_debugtoolbar import DebugToolbarExtension
-from wtforms import StringField, FloatField, IntegerField, BooleanField
-from wtforms.validators import InputRequired, Optional
+from wtforms import StringField, FloatField, IntegerField, BooleanField, RadioField
+from wtforms.validators import InputRequired, Optional, url, NumberRange
 from models import Pet, connect_db, db
 
 app = Flask(__name__)
@@ -17,25 +18,30 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 
 # creating and validating a FlaskForm class to load the add pet HTML form
+
+
 class AddPetForm(FlaskForm):
     """" Form for Adding pets to the pets data table"""
     name = StringField("Pet's name: ",
                        validators=[InputRequired()])
-    species = StringField("What species of Pet? ",
-                          validators=[InputRequired()])
-    photo_url = StringField("Photo URL: ")
+    species = RadioField("What species of Pet? ",
+                         validators=[InputRequired()],
+                         choices=[('cat', 'Cat'),
+                                  ('dog', 'Dog'),
+                                  ('porcupine', 'Porcupine')])
+    photo_url = URLField("Photo URL: ",
+                         validators=[url(), Optional(strip_whitespace=True)])
     age = IntegerField("How old is the pet? ",
-                       validators=[InputRequired()])
+                       validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes: ")
     available = BooleanField("Is this pet available for adoption? ")
-    # ,
-                            #  validators=[InputRequired()])
-
+ 
 
 @app.route('/')
 def show_homepage():
     pets = Pet.query.all()
-    return render_template('home.html', pets = pets)
+    return render_template('home.html', pets=pets)
+
 
 @app.route('/add', methods=['GET', 'POST'])    
 def add_pet_form():
@@ -48,7 +54,7 @@ def add_pet_form():
         pet = Pet(  name = form.name.data,
                     species = form.species.data,
                     photo_url = form.photo_url.data or None,
-                    age = form.age.data,
+                    age = form.age.data or None,
                     notes = form.notes.data or None,
                     available = form.available.data)
         db.session.add(pet)
@@ -56,5 +62,8 @@ def add_pet_form():
         return redirect('/')   
 
     else:
-        return render_template("addpet.html", form = form)    
+        return render_template("addpet.html", form = form)
 
+# @app.route('/<int:pet_id_number',methods=['GET', 'POST'])
+# def edit_pet_form(pet_id_number):
+    # creating edit form
--- models.py
+++ models.py
@@ -10,6 +10,7 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+
 class Pet(db.Model):
     """Pet Model"""
 
@@ -23,8 +24,7 @@ class Pet(db.Model):
     species = db.Column(db.Text, 
                         nullable=True)
     photo_url = db.Column(db.Text)
-    age = db.Column(db.Integer,
-                    nullable=False)
+    age = db.Column(db.Integer)
     notes = db.Column(db.Text)
     available = db.Column(db.Boolean,
                           nullable=False,
